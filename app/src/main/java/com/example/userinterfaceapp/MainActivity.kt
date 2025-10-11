9ff7cc6620a6da83037f6f831ed93b9ae3299d0a
Denis <chernovds21@gmail.com>
2025-10-11 20:39:25 +0300

    Переименовать макеты под фрагменты

--- app/src/main/java/com/example/userinterfaceapp/MainActivity.kt
+++ app/src/main/java/com/example/userinterfaceapp/MainActivity.kt
@@ -2,51 +2,24 @@ package com.example.userinterfaceapp
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
 
+class MainActivity : BaseActivity() {
 
-class MainActivity : AppCompatActivity() {
+    private val navController by lazy {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navHostFragment.navController
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Log.d("MainActivity", "onCreate() вызван")
-
-        // Загружаем первый фрагмент при запуске
-        if (savedInstanceState == null) {
-            navigateToOnboard()
-        }
     }
 
-    fun navigateToOnboard() {
-        Log.d("MainActivity", "Навигация к OnboardFragment")
-        replaceFragment(OnboardFragment())
-    }
-
-    fun navigateToSignIn() {
-        Log.d("MainActivity", "Навигация к SignInFragment")
-        replaceFragment(SignInFragment())
-    }
-
-    fun navigateToSignUp() {
-        Log.d("MainActivity", "Навигация к SignUpFragment")
-        replaceFragment(SignUpFragment())
-    }
-
-    fun navigateToHome() {
-        Log.d("MainActivity", "Навигация к HomeFragment")
-        replaceFragment(HomeFragment())
-    }
-
-    private fun replaceFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, fragment)
-            .addToBackStack(null)
-            .commit()
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }

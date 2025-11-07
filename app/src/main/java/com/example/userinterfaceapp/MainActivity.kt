4b2b68f2ef130a62ddf0e70a9620c32b0a4eb0f5
Denis <chernovds21@gmail.com>
2025-11-07 16:26:28 +0300

    Initial commit (clean)

---
+++ app/src/main/java/com/example/userinterfaceapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.userinterfaceapp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.userinterfaceapp.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private val logTag = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Инициализация ViewBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Log.d(logTag, "onCreate() завершён")
+
+        // A: Инициализация NavController через NavHostFragment
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+}
